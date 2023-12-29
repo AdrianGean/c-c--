7e2b5713e9500c962e3cb80baa2284e6cac1ef95
Adrian Gean <124299369+AdrianGean@users.noreply.github.com>
2023-12-28 21:22:15 -0400

    c

---
+++ au002.c
@@ -0,0 +1,11 @@
+#include <stdio.h>;
+
+int main() 
+{
+    printf("Oi, sou o irineu \n");
+    printf("Valor inteiro: %d. \n", 10);
+    printf("Valor real: %f. \n", 3.145645);
+    printf("Valor real com apenas duas casas: %.2f. \n", 3.142645);
+    printf("Dado de texto: %c. \n", 'a');
+    printf("Dado de texto: %s. \n", "testando");
+}
